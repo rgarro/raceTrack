fec3fc441de7f658e753d363dd9c47134abe2680
rgarro <rgarro@gmail.com>
2024-06-24 15:35:44 -0600

    the revolution wont be televised ...

---
+++ Assets/Scripts/BatComputer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+*      _..--""````""--.._
+*    .'       |\/|       '.
+*   /    /`._ |  | _.'\    \
+*  ;    /              \    |
+*  |   /                \   |
+*  ;  / .-.          .-. \  ;
+*   \ |/   \.-.  .-./   \| /
+*    '._       \/       _.'
+*       ''--..____..--''
+* Colonel Kaddafi has is hide out in Macondo 
+*
+*
+*@author Rolando<rolando@emptyart.xyz>
+*/
+public class BatComputer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
