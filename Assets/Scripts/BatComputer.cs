1b48a582e5d3dedb68d5eb99eb5ee699ec0bea5e
rgarro <rgarro@gmail.com>
2024-06-24 15:59:57 -0600

    kfc bbq wings ..

--- Assets/Scripts/BatComputer.cs
+++ Assets/Scripts/BatComputer.cs
@@ -11,8 +11,12 @@ using UnityEngine;
 *   \ |/   \.-.  .-./   \| /
 *    '._       \/       _.'
 *       ''--..____..--''
-* Colonel Kaddafi has is hide out in Macondo 
-*
+* 
+* el verdadero Jhonny Araya es un lumpa lumpa que se sienta cerca del Hospital SanJuan de dios
+* yo le dije que me prestara un piso en chepe para hacer un game studio  y me dijo - jala !Jala de aqui carepicha!!
+* Entonces yo jale una alas de pollo en el kfc alla por la antigua embajada americana, tal como el jefe dijo
+* un taxista pirata me llevo a un parque industrial pero el guachiman de una torre de 7 pisos queria $100000 , yo le dije que 
+* ya habia gastado el ultimo rojo en un puro de mota, el dijo que las putas de por aqui cobran peaje ... 
 *
 *@author Rolando<rolando@emptyart.xyz>
 */
